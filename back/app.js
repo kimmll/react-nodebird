0a658e76d91319a74ee8daeef4ae8cb03d7d50de
emfla <emfla@ilscp.net>
2021-10-06 10:48:48 +0900

    revise back/app.js

--- back/app.js
+++ back/app.js
@@ -37,7 +37,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 app.use(cors({
-    origin : ['http://localhost:3000', 'nodebird.com'],
+    origin : ['http://localhost:3000', 'nodebird.com', 'http://52.78.111.196/'],
     credentials : true, // credentials : true로 해야 서로 다른 도메인간 쿠키가 전달됨
 }))
 
